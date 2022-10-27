d079b4a5a506df89c3cc5d65ce588455cc04ff8a
Brendan Devlin <52110466+Donttestturing@users.noreply.github.com>
2022-10-26 19:27:35 -0700

    Create change image functionality

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,25 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+ 
+  const soundChoice = document.getElementById('horn-select'); 
+
+
+  soundChoice.addEventListener('change', (event) => {
+        
+      const displayImage = document.getElementsByTagName('img');   //img ref
+      switch(soundChoice.value){
+        case 'air-horn':
+          displayImage[0].src = 'assets/images/air-horn.svg';
+          break;
+        case 'car-horn':
+          displayImage[0].src = 'assets/images/car-horn.svg';
+          break;
+        case 'party-horn':
+          displayImage[0].src = 'assets/images/party-horn.svg';
+          break;
+      }
+  });
+
+
 }
