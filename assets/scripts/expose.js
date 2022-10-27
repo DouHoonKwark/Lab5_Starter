ea99498148fe30751d745b2af11aab3b0ef65715
Brendan Devlin <52110466+Donttestturing@users.noreply.github.com>
2022-10-26 23:44:13 -0700

    Add button functionality

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,6 +5,10 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
  
   const soundChoice = document.getElementById('horn-select'); 
+  const soundChosen = document.querySelector("[class='hidden']");   //get audio element
+
+  const buttons = document.getElementsByTagName('button')     //get button
+  const jsConfetti = new JSConfetti()                       //confetti object
 
 
   soundChoice.addEventListener('change', (event) => {
@@ -13,15 +17,34 @@ function init() {
       switch(soundChoice.value){
         case 'air-horn':
           displayImage[0].src = 'assets/images/air-horn.svg';
+          soundChosen.src = 'assets/audio/air-horn.mp3';
           break;
         case 'car-horn':
-          displayImage[0].src = 'assets/images/car-horn.svg';
+          displayImage[0].src = 'assets/images/car-horn.svg'; 
+          soundChosen.src = 'assets/audio/car-horn.mp3';
           break;
         case 'party-horn':
           displayImage[0].src = 'assets/images/party-horn.svg';
+          soundChosen.src = 'assets/audio/party-horn.mp3';
           break;
       }
+
   });
+
+  buttons[0].addEventListener('click', (event) => {           //button gets clicked event, receiving correctly, need to play
+    if(soundChoice.value == 'party-horn'){
+      jsConfetti.addConfetti();
+    }
+    soundChosen.play(); //play sound 
+  });
+
+
+
+
+
+
+
+
 
 
 }
